fe2e1ca1d428bbf16e999466399d6928ad4bd589
JCantrell93 <jackfcantrell@gmail.com>
2025-05-27 20:24:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function checkPassword() {
     alert("Incorrect password. Try again.");
   }
 }
+
+function playMusic() {
+  const container = document.getElementById("audio-container");
+  container.innerHTML = `
+    <iframe
+      width="0"
+      height="0"
+      src="https://www.youtube.com/embed/hw7I2GehPUk?start=15&autoplay=1&loop=1&playlist=hw7I2GehPUk"
+      frameborder="0"
+      allow="autoplay"
+    ></iframe>
+  `;
+}
